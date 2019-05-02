29c506fd20769ad684bbd937b71f4ab2e0e639f9
oahmaro <osamaahmaro@gmail.com>
2019-05-02 14:40:42 +0300

    Creating the JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb+srv://osama:Osama12@devconnector-8cd0b.mongodb.net/test?retryWrites=true"
-};
+  mongoURI:
+    'mongodb+srv://osama:Osama12@devconnector-8cd0b.mongodb.net/test?retryWrites=true',
+  secretOrKey: 'secret'
+}
