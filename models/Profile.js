d88fe9540bf2cb819c55759c7afa81f91297c9e0
oahmaro <osamaahmaro@gmail.com>
2019-05-04 16:55:07 +0300

    Current User Profile

--- models/Profile.js
+++ models/Profile.js
@@ -36,57 +36,61 @@ const ProfileSchema = new Schema({
     type: String
   },
   experience: [
-    title: {
-      type: String,
-      required: true
-    },
-    company: {
-      type: String,
-      required: true
-    },
-    location: {
-      type: String
-    },
-    from: {
-      type: Date,
-      required: true
-    },
-    to: {
-      type: Date
-    },
-    current: {
-      type: Boolean,
-      default: false
-    },
-    description: {
-      type: String
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
     }
   ],
   education: [
-    school: {
-      type: String,
-      required: true
-    },
-    degree: {
-      type: String,
-      required: true
-    },
-    fieldofstudy: {
-      type: String
-    },
-    from: {
-      type: Date,
-      required: true
-    },
-    to: {
-      type: Date
-    },
-    current: {
-      type: Boolean,
-      default: false
-    },
-    description: {
-      type: String
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
     }
   ],
   social: {
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,41 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+// Load Profile Model
+const Profile = require('../../models/Profile')
+
+// Load User Model
+const User = require('../../models/User')
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
-
 router.get('/test', (req, res) => {
   return res.json({
-    msg: "Profile Works"
+    msg: 'Profile Works'
   })
-});
+})
 
-module.exports = router;
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {}
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user'
+          res.status(404).json(errors)
+        }
+      })
+      .catch(err => res.status(404).json(err))
+  }
+)
+
+module.exports = router
