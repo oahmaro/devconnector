cbc2494a949f614f27695f5fd6021419d1c94bcf
oahmaro <osamaahmaro@gmail.com>
2019-05-05 22:54:11 +0300

    Post Create Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,47 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+// Post model
+const Post = require('../../models/Post')
+
+// Load Validation
+const validatePostInput = require('../../validation/post')
 
 // @route   GET api/post/test
 // @desc    Tests posts route
 // @access  Public
 router.get('/test', (req, res) => {
   return res.json({
-    msg: "Posts Works"
+    msg: 'Posts Works'
   })
-});
+})
 
-module.exports = router;
+// @route   POST api/post
+// @desc    Create post
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body)
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors)
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    })
+
+    newPost.save().then(post => res.json(post))
+  }
+)
+
+module.exports = router
