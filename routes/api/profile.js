8efffe35d47c3e2bbce8bcf545729dd120602067
oahmaro <osamaahmaro@gmail.com>
2019-05-05 09:09:17 +0300

    Profile Field Validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
 
+// Load Validation
+const validatorProfileInput = require('../../validation/profile')
+
 // Load Profile Model
 const Profile = require('../../models/Profile')
 
@@ -26,8 +29,8 @@ router.get(
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const errors = {}
-
     Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar'])
       .then(profile => {
         if (!profile) {
           errors.noprofile = 'There is no profile for this user'
@@ -45,6 +48,14 @@ router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatorProfileInput(req.body)
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors)
+    }
+
     // Get fields
     const profileFields = {}
     profileFields.user = req.user.id
