e07468c5dd4ad528c53a35f9ed058159dd841713
oahmaro <osamaahmaro@gmail.com>
2019-05-06 11:34:10 +0300

    Reach Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import './App.css'
+import { Router } from '@reach/router'
+
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
+import Register from './components/oath/Regitser'
+import Login from './components/oath/Login'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Landing />
-      <Footer />
+      <Router>
+        <Landing path="/" />
+      </Router>
+      <div className="container">
+        <Router>
+          <Register path="/register" />
+          <Login path="/login" />
+        </Router>
+      </div>
+      <Footer default />
     </div>
   )
 }
