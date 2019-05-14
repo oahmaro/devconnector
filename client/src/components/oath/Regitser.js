c69ca5ea66e3cecb952f690d2b12780d51213e73
oahmaro <osamaahmaro@gmail.com>
2019-05-14 12:18:32 +0300

    Redux Action / Reducer Workflow Example

--- client/src/components/oath/Regitser.js
+++ client/src/components/oath/Regitser.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import PropTypes from 'prop-types'
+// import axios from 'axios'
 import classnames from 'classnames'
+import { connect } from 'react-redux'
+import { registerUser } from '../../actions/authActions'
 
 class Register extends Component {
   state = {
@@ -24,17 +27,20 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     }
-    axios
-      .post('api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }))
+
+    this.props.registerUser(newUser)
+    // axios
+    //   .post('api/users/register', newUser)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => this.setState({ errors: err.response.data }))
   }
 
   render() {
     const { errors } = this.state
-
+    const { user } = this.props.auth
     return (
       <div className="register">
+        {JSON.stringify(user)}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -117,4 +123,16 @@ class Register extends Component {
   }
 }
 
-export default Register
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(Register)
